b8a4d818ff9ecb182dd4d5d17f930c80c5599138
DESKTOP-CRCM38O\Kasia <kasia2011r@gmail.com>
2023-12-06 11:04:28 +0100

    Implement ViewPropertyDetails Action and PropertyDetailsModel

--- Controllers/PropertyListingController.cs
+++ Controllers/PropertyListingController.cs
@@ -5,25 +5,31 @@ namespace HolidayProject.Controllers
 {
     public class PropertyListingController : Controller
     {
-        private List<PropertyListingModel> properties = new List<PropertyListingModel>
+        private List<PropertyDetailsModel> properties = new List<PropertyDetailsModel>
         {
-            new PropertyListingModel
+            new PropertyDetailsModel
             {
                 Id = 1,
                 Name = "Rose Cottage",
                 Blurb = "Beautiful cottage on the Cornwall coast",
                 Location = "Cornwall",
                 NumberOfBedrooms = 3,
-                CostPerNight = 350
+                CostPerNight = 350,
+                Description = "A charming cottage with stunning views.",
+                Amenities = new List<string> { "WiFi", "Parking", "Garden" },
+                BookedDates = new List<DateTime> { DateTime.Now.AddDays(5), DateTime.Now.AddDays(10) }
             },
-            new PropertyListingModel
+            new PropertyDetailsModel
             {
                 Id = 2,
                 Name = "Saffron House",
                 Blurb = "Stately home on the Devon moors",
                 Location = "Devon",
                 NumberOfBedrooms = 7,
-                CostPerNight = 730
+                CostPerNight = 730,
+                Description = "A grand stately home with luxurious amenities.",
+                Amenities = new List<string> { "Pool", "Tennis Court", "Sauna" },
+                BookedDates = new List<DateTime> { DateTime.Now.AddDays(8), DateTime.Now.AddDays(15) }
             }
         };
 
@@ -35,6 +41,12 @@ namespace HolidayProject.Controllers
         public IActionResult ListAvailable(DateTime start, DateTime end)
         {
             return View("ListProperties", properties);
+        }
+
+        [HttpGet]
+        public IActionResult ViewPropertyDetails([FromRoute] int id)
+        {
+            return View();
         }
     }
 }
